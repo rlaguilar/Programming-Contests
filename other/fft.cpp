90a73cb1350b1c2be4efe7c5ee3211342728cc75
rlac1990 <rlac1990@gmail.com>
2014-12-03 08:26:25 -0500

    Update fft.cpp

--- other/fft.cpp
+++ other/fft.cpp
@@ -1,15 +1,14 @@
 // the glory is for GOD
 
 /*
-	Name: Reynaldo Lázaro Aguilar Casajuana
-	Author: rlac
-	Date: 22/11/12 16:37
-	Description: Fast Fourier Transform (FFT)
-	Verified:	
-	SPOJ 31. Fast Multiplication
-	SPOJ 235. Very Fast Multiplication
-	COJ  1760 - Triple Sums
-	SPOJ 8372 - TSUM
+    Name: Reynaldo Lázaro Aguilar Casajuana
+    Author: rlac
+    Date: 22/11/12 16:37
+    Description: Fast Fourier Transform (FFT)
+    Verified:	SPOJ 31. Fast Multiplication
+                SPOJ 235. Very Fast Multiplication
+                COJ  1760 - Triple Sums
+                SPOJ 8372 - TSUM
 */
 
 #include <bits/stdc++.h>
@@ -33,64 +32,64 @@ int rev[MAXN];
 /* for inverse fft, theta = -2 * PI / n */
 void fft (point a[], double theta, int n)
 {
-	for (int m = n; m >= 2; m >>= 1, theta *= 2)
-	{
-		int mh = m >> 1;
+    for (int m = n; m >= 2; m >>= 1, theta *= 2)
+    {
+        int mh = m >> 1;
 
-		for (int i = 0; i < mh; ++i)
-		{
-			point w = polar(1.0, i * theta);
+        for (int i = 0; i < mh; ++i)
+        {
+            point w = polar(1.0, i * theta);
 
-			for (int j = i; j < n; j += m)
-			{
-				int k = j + mh;
-				point x = a[j] - a[k];
-				a[j] += a[k];
-				a[k] = w * x;
-			}
-		}
-	}
+            for (int j = i; j < n; j += m)
+            {
+                int k = j + mh;
+                point x = a[j] - a[k];
+                a[j] += a[k];
+                a[k] = w * x;
+            }
+        }
+    }
 
-	for (int j = 1; j < n - 1; ++j)
-	{
-		int idx = rev[j];
-		
-		if (j < idx)
-			swap(a[j], a[idx]);
-	}
+    for (int j = 1; j < n - 1; ++j)
+    {
+        int idx = rev[j];
+        
+        if (j < idx)
+            swap(a[j], a[idx]);
+    }
 }
 
 /* This function multiplies two given polynomials. A polynomial is represented as: p[i] * (x ^ i)	*/
 void multiply (point p1[], point p2[], int N)
 {
-	for (int j = 1, i = 0; j < N - 1; ++j)
+    for (int j = 1, i = 0; j < N - 1; ++j)
     {
-		for (int k = N >> 1; k > (i ^= k); k >>= 1);
-		
-		rev[j] = i;
-	}
-	
+        for (int k = N >> 1; k > (i ^= k); k >>= 1);
+        
+        rev[j] = i;
+    }
+    
     double theta = 2 * M_PI / N, inv = 1.0 / N;
     fft(p1, theta, N);
-	fft(p2, theta, N);
+    fft(p2, theta, N);
 
-	for (int i = 0; i < N; ++i)
-	    p1[i] *= p2[i];
+    for (int i = 0; i < N; ++i)
+        p1[i] *= p2[i];
 
-	fft(p1, -theta, N);
+    fft(p1, -theta, N);
 
-	for (int i = 0; i < N; ++i)
-	    p1[i] *= inv;
-	    
-	// at this point p1 is the resultant polynomial
+    for (int i = 0; i < N; ++i)
+        p1[i] *= inv;
+        
+    // at this point p1 is the resultant polynomial
 }
 
 int main ()
 {
-	//Improvement region
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	
-	return 0;
+    //Improvement region
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    return 0;
 }
 
